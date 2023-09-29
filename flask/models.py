de0806c382fb87557243df28a2ab10048be0c832
LoganWilliams1 <loganwilliamslw1@gmail.com>
2023-09-29 17:08:53 -0400

    added basic backend authentication

--- flask/models.py
+++ flask/models.py
@@ -10,4 +10,7 @@ def get_uuid():
 
 class User(db.Model):
     __tablename__ = "users"
-    id = db.Column(db.String(32), primary_key=True, unique=True, defualt=get_uuid)
+    id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid)
+    email = db.Column(db.String(345), unique=True)
+    password = db.Column(db.Text, nullable=False)
+
