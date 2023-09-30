091a3ed648267a6c39ecdf1e2148bf3dca689212
LoganWilliams1 <loganwilliamslw1@gmail.com>
2023-09-30 02:40:35 -0400

    added router page, changed index.jsx to render router instead of app

---
+++ src/Router.jsx
@@ -0,0 +1,15 @@
+import {BrowserRouter, Switch, Route } from "react-router-dom";
+import {App} from "./App";
+
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={App}/>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Router
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
+import Router from "./Router";
+import "./style.css"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Router />
+    </React.StrictMode>,
+    document.getElementById('root'));
